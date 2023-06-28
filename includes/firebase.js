b697a13664277bd0ada73f3aa9a7fc106f3828b1
JackeL <it.davidkaiser@gmail.com>
2023-06-27 17:40:54 -0700

    Update firebase.js

--- includes/firebase.js
+++ includes/firebase.js
@@ -5,10 +5,10 @@ import { getAuth, onAuthStateChanged, sendEmailVerification, createUserWithEmail
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: "boyer-f35f5",
-  storageBucket: "boyer-f35f5.appspot.com",
-  messagingSenderId: "947602977493",
-  appId: "1:947602977493:web:b5e239fb62ff08d5a4e85c"
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
